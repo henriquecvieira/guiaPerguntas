924e206f68106600bfeb308ac600becb46a7d5df
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-21 17:04:15 -0300

    aula 37 formação NODE JS

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
+const bodyParser = require("body-parser")
 
+//estou dizendo para o express usar o EJS como View Engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('./public'))
 
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Rotas
 app.get("/", (req, res)=>{  
     res.render("index")     
 })
@@ -12,5 +19,11 @@ app.get("/perguntar", (req, res)=>{
     res.render("perguntar")     
 })
 
-app.listen(8080, ()=>{console.log("App rodando!")
+app.post("/salvarpergunta", (req, res)=>{  
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send("formulário recebido! título " + titulo + " " + "descrição" + descricao)         
+})
+
+app.listen(8080, ()=>{console.log("Back end started at http://localhost:8080!")
 })
