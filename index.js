6bbb806a28abaa23c1b72a8864c4f06cb5826165
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-03-24 12:13:15 -0300

    enviando as prguntas para o banco de dados

--- index.js
+++ index.js
@@ -67,6 +67,17 @@ app.get("/pergunta/:id",(req, res) => {
         res.redirect("/");
     }
 });
+});
+
+app.post("/responder", (req, res) =>{
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    });
 })
 
 app.listen(8080, ()=>{console.log("Back end started at http://localhost:8080!");})
